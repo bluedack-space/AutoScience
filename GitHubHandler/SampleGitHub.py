cc3365bb7ffd175643ee76f825bc512e4617af42
bluedack-space <71576595+bluedack-space@users.noreply.github.com>
2022-06-23 22:48:11 +0900

    Add files via upload

--- GitHubHandler/SampleGitHub.py
+++ GitHubHandler/SampleGitHub.py
@@ -2,32 +2,59 @@ import base64
 import os
 from github import Github
 
-token      = '[Setting]>[Developer Setting]'
+class GitHubHandler():
+
+    github = None
+    
+    def __init__(self,token):
+        self.Github(token)
+
+    def __del__(self):
+        pass
+
+    def Github(self,token):
+        self.github = Github(token)
+    
+    def getRepository(self,repository):
+        return self.github.get_repo(repository)
+
+    def getDirectoryContents(self,repo,dirName):
+        dirContents = repo.get_dir_contents(dirName)
+        return dirContents
+    
+    def getFileNameList(self,dirContents,extList):
+        fileNameList = []
+        for i in range(len(dirContents)):
+            fileNameExist = dirContents[i].name
+            root, ext = os.path.splitext(fileNameExist)
+            if ext in extList:
+                print("########"+fileNameExist)
+                fileNameList.append(fileNameExist)
+        return fileNameList
+
+    def downloadAll(self,dirContents,fileNameList):
+
+        for n in range(len(fileNameList)):
+            fileName = fileNameList[n]
+
+            sha = 0
+            for i in range(len(dirContents)):
+                if dirContents[i].name == fileName:
+                    sha = dirContents[i].sha
+
+            blob    = repo.get_git_blob(sha)
+            content = base64.b64decode(blob.content)
+
+            with open(fileName, mode="wb") as f:
+                f.write(content)
+
+token      = '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'
 repository = 'bluedack-space/AutoScience'
 dirName    = '/Image'
+extList    = ['.jpeg','.jpg']
 
-g            = Github(token)
-repo         = g.get_repo(repository)
-dir_contents = repo.get_dir_contents(dirName)
-
-fileNameListImage = []
-for i in range(len(dir_contents)):
-    fileNameExist = dir_contents[i].name
-    root, ext = os.path.splitext(fileNameExist)
-    print(ext)
-    if ext == '.jpeg' or ext == '.jpg':
-        fileNameListImage.append(fileNameExist)
-
-for n in range(len(fileNameListImage)):
-    fileName = fileNameListImage[n]
-
-    sha = 0
-    for i in range(len(dir_contents)):
-        if dir_contents[i].name == fileName:
-            sha = dir_contents[i].sha
-
-    blob    = repo.get_git_blob(sha)
-    content = base64.b64decode(blob.content)
-
-    with open(fileName, mode="wb") as f:
-        f.write(content)
+gitHdl            = GitHubHandler( token )
+repo              = gitHdl.getRepository( repository )
+dirContents       = gitHdl.getDirectoryContents( repo, dirName )
+fileNameList      = gitHdl.getFileNameList( dirContents, extList )
+gitHdl.downloadAll( dirContents, fileNameList )
