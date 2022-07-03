2df4351f8fd5adc44576a28686a99baa60a6b8a0
bluedack-space <71576595+bluedack-space@users.noreply.github.com>
2022-07-03 12:09:29 +0900

    Add files via upload

--- GoogleSpreadSheet/GoogleSpreadSheetHandler.py
+++ GoogleSpreadSheet/GoogleSpreadSheetHandler.py
@@ -1,26 +1,35 @@
-
-# Ref https://www.twilio.com/blog/an-easy-way-to-read-and-write-to-a-google-spreadsheet-in-python-jp
-
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 
-# use creds to create a client to interact with the Google Drive API
-scope  =['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
+scope  = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
 creds  = ServiceAccountCredentials.from_json_keyfile_name('client_secret.json', scope)
 client = gspread.authorize(creds)
 
-# Find a workbook by name and open the first sheet
-# Make sure you use the right name here.
-sheet = client.open("AutoScience").sheet1
+sheet  = client.open("AutoScience").sheet1
 
-# Extract and print all of the values
-list_of_hashes = sheet.get_all_records()
-print(list_of_hashes)
+#list_of_hashes = sheet.get_all_records()
+#print(list_of_hashes)
 
-# Inserting Row Data
-row   = ["I'm","inserting","a","row","into","a,","Spreadsheet","with","Python"]
-index = 10
-sheet.insert_row(row, index)
+import numpy as np
+y1 = np.random.rand()
+y2 = np.random.rand()
+#sheet.insert_row([0.1,1.0,2.0],numRow+1)
+sheet.append_row([0.1,y1,y2])
 
-# Inserting Cell Data
-sheet.update_cell(11, 1, "I just wrote to a spreadsheet using Python!")
+xList = []
+y1List = []
+y2List = []
+for i in range(50):
+    x  = sheet.cell(i+1,1).value
+    y1 = sheet.cell(i+1,2).value
+    y2 = sheet.cell(i+1,3).value
+    xList.append(x)
+    y1List.append(y1)
+    y2List.append(y2)
+
+print(xList)
+print(y1List)
+
+#import plotly.express as px
+#fig = px.scatter(x=xList, y=y1List)
+#fig.show()
